02eb16a44103ef8722ab4e68ae109911d52f3b88
Venkadesh-coder <54761484+Venkadesh-coder@users.noreply.github.com>
2020-07-19 23:05:11 +0530

    Add files via upload

---
+++ Ascending order/ascend_order.js
@@ -0,0 +1,18 @@
+function ascend_order(array)
+{
+    var count=array.length - 1,swap,j,i;
+    for(j=0;j<count;j++)
+    {
+        for(i=0;i<count;i++)
+        {
+            if(array[i]>array[i+1])
+            {
+                swap=array[i+1];
+                array[i+1]=array[i];
+                array[i]=swap;
+            }
+        }
+    }
+    return array;
+}
+console.log(ascend_order([4, 10, 2, 9, 11, 3, 13, 5]));
