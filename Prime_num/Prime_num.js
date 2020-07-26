8c5ce5e0dc3e270176f66db926d7f0de1f50ace8
Venkadesh-coder <54761484+Venkadesh-coder@users.noreply.github.com>
2020-07-26 22:35:09 +0530

    Add files via upload

---
+++ Prime_num/Prime_num.js
@@ -0,0 +1,8 @@
+var numArray=[1,2,3,4,5,6,7,8,9,10,12,14,13];
+numArray=numArray.filter((number)=>{
+  for(var i=2;i<=Math.sqrt(number);i++){
+    if(number%i===0) return false;
+  }
+  return true;
+});
+console.log(numArray);
